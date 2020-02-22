e5911ef061551da1289d4948e4690f33a940c6c1
mregger <eduardoegger@gmail.com>
2020-02-21 21:48:12 -0400

    fix(main.py) load model before h() to avoid exponential increase in h

--- fuselageOptimisation/main.py
+++ fuselageOptimisation/main.py
@@ -1,9 +1,16 @@
+import matplotlib.pyplot as plt
 from vsp_interface import VspModel
 
 filename = '/home/mregger/Documents/HardwareStuff/kontor/design/openVSP/kontor.vsp3'
 
-t = 50      # number of iterations
-c = 2   # change factor (multiply values by this)
+performance = {
+    'h': [],
+    'c': [],
+    't': []
+}
+
+t = 100      # number of iterations
+c = 0.40     # change factor (multiply values by this)
 
 h = 0         # current heuristic value
 old_h = None  # previous heuristic value
@@ -15,11 +22,11 @@ cockpit_min_width = 2
 cockpit_min_height = 3.5
 nose_min_height = 3
 
+model = VspModel(filename)
 while t > 0:
     pass
     i = 0
     # open file
-    model = VspModel(filename)
 
     # modify by constant
     base_sections = model['FuselageBase']['XSecCurve']
@@ -30,7 +37,7 @@ while t > 0:
             pass
             if 'Width' in element:
                 pass
-                value = model['FuselageBase']['XSecCurve'][i][element]['value'] * c
+                value = model['FuselageBase']['XSecCurve'][i][element]['value'] * (c + 1)
                 id = model['FuselageBase']['XSecCurve'][i][element]['_id']
 
                 if i <= 2 and value > tail_min_width:
@@ -39,7 +46,7 @@ while t > 0:
                     model.set_param(id=id, value=value)
 
             elif 'Height' in element:
-                value = model['FuselageBase']['XSecCurve'][i][element]['value'] * c
+                value = model['FuselageBase']['XSecCurve'][i][element]['value'] * (c + 1)
                 id = model['FuselageBase']['XSecCurve'][i][element]['_id']
 
                 if i <= 5 and value > cockpit_min_height:
@@ -53,31 +60,40 @@ while t > 0:
             pass
             if 'Width' in element:
                 pass
-                value = model['TailFuselage']['XSecCurve'][i][element]['value'] * c
+                value = model['TailFuselage']['XSecCurve'][i][element]['value'] * (1 + c)
                 id = model['TailFuselage']['XSecCurve'][i][element]['_id']
 
                 if value > 0.5:
                     model.set_param(id=id, value=value)
 
             elif 'Height' in element:
-                value = model['TailFuselage']['XSecCurve'][i][element]['value'] * c
+                value = model['TailFuselage']['XSecCurve'][i][element]['value'] * (1 + c)
                 id = model['TailFuselage']['XSecCurve'][i][element]['_id']
 
                 if value > 0.5:
                     model.set_param(id=id, value=value)
 
     model.save_file(filename=filename)
+    model = VspModel(filename)
     # run heuristic
     h = model.h()
+    print('C: ', c, ' h: ', h, ' t: ', t)
+    performance['h'].append(h)
+    performance['c'].append(c+1)
+    performance['t'].append(t)
     # if heuristic is better than before, continue. Else change direction
     if old_h is not None and old_h < h:
         pass
         print('Wrong way, turn around...')
-        c *= -0.95
+        c *= -0.99
     elif old_h is not None and old_h >= h:
         pass
         print('Getting closer...')
-        c *= 0.95
+        c *= 0.99
     old_h = h
-    print('C: ', c, ' h: ', h, ' t: ', t)
     t -= 1
+
+plt.plot(performance['h'], label='h')
+plt.plot(performance['c'], label='c')
+plt.savefig('r.png')
+plt.clf()
